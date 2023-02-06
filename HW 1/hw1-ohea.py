4ce3dabeb99b12df0552b7540f7ef169aca7fc32
Yes <85800291+YesVRC@users.noreply.github.com>
2023-02-06 01:16:21 -0500

    deleted:    Class: 1/Instructions.txt 	new file:   HW 1/cs104 hw1-ohea.pdf 	new file:   HW 1/hw1-ohea.py

---
+++ HW 1/hw1-ohea.py
@@ -0,0 +1,8 @@
+print('This is my first homework')
+print('It is kind of a boring program but I am making a computer do something and that is really cool')
+print('5+4')
+print(5+4)
+num1=10
+num2=20
+print(num1+num2)
+#cs104
